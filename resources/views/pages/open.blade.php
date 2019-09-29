9b0318bc6fc9260ee3177d9835ad32b42c43d25d
sajjad ali <asajjad201@gmail.com>
2019-09-29 14:03:16 +0500

    FIVE

--- resources/views/pages/open.blade.php
+++ resources/views/pages/open.blade.php
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="container">
                 @extends('name')
-                New Text Added ONE TWO THREE FOUR
+                New Text Added ONE TWO THREE FOUR FIVE
             </div>
         </div>
     </div>
