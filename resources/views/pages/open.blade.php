49ebced8241f248ba825b7d65b356d9c2c3f5bce
sajjad ali <asajjad201@gmail.com>
2019-09-29 13:45:40 +0500

    two

--- resources/views/pages/open.blade.php
+++ resources/views/pages/open.blade.php
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="container">
                 @extends('name')
-                New Text Added ONE
+                New Text Added ONE TWO
             </div>
         </div>
     </div>
