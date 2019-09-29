e0a472456b1caab0e49fb859fc1f5cd8bd8a5671
sajjad ali <asajjad201@gmail.com>
2019-09-29 15:41:27 +0500

    This is first master branch to merge

--- resources/views/pages/open.blade.php
+++ resources/views/pages/open.blade.php
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="container">
                 @extends('name')
-                New Text Added ONE TWO THREE FOUR FIVE SIX
+                This is master branch-ONE
             </div>
         </div>
     </div>
